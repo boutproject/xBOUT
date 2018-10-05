93f7a24016ac87e5dd1c5ebf3e0359c20735df3e
Thomas Nicholas <tegn500@york.ac.uk>
2018-10-05 16:32:56 +0100

    Added some new WIP examples

---
+++ examples/parameter_scan.py
@@ -0,0 +1,16 @@
+from xarray import concat
+from xcollect.boutdataset import BoutDataset
+
+# Parameter which was scanned over
+p = [0.5, 1.0, 1.5]
+# The directories to find the results in
+datapaths = ['./' + str(pressure) for pressure in p]
+
+# Load the results of each run as a separate dataset
+boutdatasets = [BoutDataset(datapath=path) for path in datapaths]
+
+# We  can now merge these into a single dataset along a new dimension
+ds = concat(datasets, 'p', p)
+
+# Plot results over the scanned parameter
+
---
+++ examples/redistribute_restarts.py
@@ -0,0 +1,15 @@
+from xcollect.boutdataset import BoutDataset
+
+
+bd = BoutDataset(datapath='.')
+
+# Multiply density and temperature fluctuations by 2
+for var in ['n', 'T']:
+    bd.data['var'] = bd.data['var'] * 2
+
+# Downsample the data by interpolating onto a coarser x coordinate vector
+new_x = bd.data['x'].slice(None,None,3)
+bd.data = bd.data.interp(x=new_x)
+
+# Save new restart files with a different parallelisation
+bd.save_restart(savepath='.', nxpe=4, nype=8)
