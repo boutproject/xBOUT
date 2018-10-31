bf8f459addc7a19290126b8b5fafcf303f64d9f2
Thomas Nicholas <tegn500@york.ac.uk>
2018-10-31 13:28:12 +0000

    example of how to get the numpy ndarray directly

--- examples/luddite.py
+++ examples/luddite.py
@@ -1,10 +1,10 @@
-from xcollect.boutdataset import BoutDataset
+from xcollect.boutdataset import open_boutdataset
 
 
-ds = BoutDataset(datapath='.')
+ds = open_boutdataset(datapath='.')
 
 # To access just the desired numpy array values directly, slice then use .values
 n = ds['n'].isel(t=slice(10,None,5)).values
 
 # The dataset can also be loaded without using dask, though this will be far more memory-intensive
-ds = BoutDataset(datapath='.', chunks=None)
+ds = open_boutdataset(datapath='.', chunks=None)
