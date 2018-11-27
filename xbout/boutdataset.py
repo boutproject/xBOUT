94b936296b4d010d49dfc8b8f35532d6079ab9c5
Thomas Nicholas <tegn500@york.ac.uk>
2018-11-27 19:01:46 +0000

    Option to plot separatrix with imshow

--- xbout/boutdataset.py
+++ xbout/boutdataset.py
@@ -4,6 +4,7 @@ from dask.diagnostics import ProgressBar
 
 from pprint import pprint
 from pathlib import Path
+import warnings
 
 from boutdata.data import BoutOptionsFile
 
@@ -308,7 +309,7 @@ class BoutAccessor(object):
                                          axis=ax, origin='lower', **kwargs)
 
         cbar = plt.colorbar(imshow_block.im, ax=ax)
-        cbar.ax.set_ylabel(variable, rotation=0)
+        cbar.ax.set_ylabel(variable)
 
         timeline = amp.Timeline(np.arange(data.sizes[animate_over]), fps=fps)
 
@@ -319,6 +320,10 @@ class BoutAccessor(object):
         ax.set_title("{} variation over {}".format(variable, animate_over))
         ax.set_xlabel(x)
         ax.set_ylabel(y)
+
+        # Plot separatrix
+        if sep_pos:
+            ax = plot_separatrix(data, sep_pos, ax)
 
         if animate:
             anim.controls(timeline_slider_args={'time_label': animate_over})
@@ -331,3 +336,35 @@ def _find_time_dependent_vars(data):
     evolving_vars = set(var for var in data.data_vars if 't' in data[var].dims)
     time_independent_vars = set(data.data_vars) - set(evolving_vars)
     return list(evolving_vars), list(time_independent_vars)
+
+
+def plot_separatrix(da, sep_pos, ax, radial_coord='x'):
+    """
+    Plots the separatrix as a black dotted line.
+
+    Should plot in the correct place regardless of the choice of coordinates
+    for the plot axes, and the type of plot. sep_x needs to be supplied as the
+    integer index of the grid point location of the separatrix.
+    """
+
+    # TODO Maybe use a decorator to do this for any type of plot?
+
+    # 2D domain needs to intersect the separatrix plane to be able to plot it
+    dims = da.dims
+    if radial_coord not in dims:
+
+        warnings.warn("Cannot plot separatrix as domain does not cross "
+                      "separatrix, as it does not have a radial dimension",
+                      Warning)
+        return
+
+    else:
+        # Determine the separatrix position in terms of the radial coordinate
+        # being used in the plot
+        x_coord_vals = da.coords[radial_coord].values
+        sep_x_pos = x_coord_vals[sep_pos]
+
+        # Plot a vertical line at that location on the plot
+        ax.axvline(x=sep_x_pos, linewidth=2, color="black", linestyle='--')
+
+        return ax
