26b226a7666de46d6eca3453db59f247db513742
Thomas Nicholas <tegn500@york.ac.uk>
2019-07-09 13:33:46 +0100

    Added test for registering new geometry

--- xbout/tests/test_geometries.py
+++ xbout/tests/test_geometries.py
@@ -1,7 +1,9 @@
-from xbout.geometries import apply_geometry
-from xbout.geometries import UnregisteredGeometryError
-
+from xarray import Dataset, DataArray
+from xarray.testing import assert_equal
 import pytest
+
+from xbout.geometries import (apply_geometry, register_geometry,
+    UnregisteredGeometryError, REGISTERED_GEOMETRIES)
 
 
 class TestGeometryRegistration:
@@ -10,3 +12,17 @@ class TestGeometryRegistration:
                            match="tesseract is not a registered geometry"):
             apply_geometry(ds=None, geometry_name="tesseract")
 
+    def test_register_new_geometry(self):
+        @register_geometry(name="Schwarzschild")
+        def add_schwarzschild_coords(ds):
+            ds['event_horizon'] = 4.0
+            return ds
+
+        assert "Schwarzschild" in REGISTERED_GEOMETRIES.keys()
+
+        original = Dataset()
+        updated = apply_geometry(ds=original, geometry_name="Schwarzschild")
+        assert_equal(updated['event_horizon'], DataArray(4.0))
+
+        # clean up
+        del REGISTERED_GEOMETRIES["Schwarzschild"]
