7dcaf4437e258820522ed265ff1b946465af0989
John Omotani <john.omotani@ukaea.uk>
2019-08-05 13:49:38 +0100

    Use pcolormesh instead of imshow for 2d animations

--- xbout/__init__.py
+++ xbout/__init__.py
@@ -6,7 +6,7 @@ from .geometries import register_geometry, REGISTERED_GEOMETRIES
 from .boutdataset import BoutDatasetAccessor
 from .boutdataarray import BoutDataArrayAccessor
 
-from .plotting.animate import animate_imshow
+from .plotting.animate import animate_pcolormesh
 from .plotting.utils import plot_separatrix
 
 from ._version import __version__
