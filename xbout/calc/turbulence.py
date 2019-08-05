62df7d64b3803219ecfd9feee4a3376c3b8bc685
Thomas Nicholas <tegn500@york.ac.uk>
2019-08-05 13:59:15 +0100

    Added rms as example analysis function

---
+++ xbout/calc/turbulence.py
@@ -0,0 +1,32 @@
+import numpy as np
+import xarray as xr
+
+
+# TODO write a decorator to add functions as accessors to dataarrays?
+
+
+def rms_gufunc(x):
+    """Generalized ufunc to calculate root mean squared value of an array."""
+    squares = np.square(x)
+    return np.sqrt(np.mean(squares, axis=-1))
+
+
+def rms(da, dim=None, dask='parallelized', keep_attrs=True):
+    """
+    Reduces a dataarray by calculating the root mean square along the dimension
+    dim.
+    """
+
+    # TODO If dim is None then take the root mean square along all dimensions?
+    if dim is None:
+        raise ValueError('Must supply a dimension along which to calculate rms')
+
+    rms = xr.apply_ufunc(rms_gufunc, da,
+                         input_core_dims=[[dim]],
+                         dask=dask, output_dtypes=[da.dtype],
+                         keep_attrs=keep_attrs)
+
+    # Return the name of the da as variable_rms
+    rms.name = str(da.name) + '_rms'
+
+    return rms
