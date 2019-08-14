ca0cf2490fcccc9f5dd8edeec90638b88ba96665
John Omotani <john.omotani@ukaea.uk>
2019-08-14 21:37:06 +0100

    Test both dimensions in test_reduce_2d

--- xbout/calc/tests/test_turbulence.py
+++ xbout/calc/tests/test_turbulence.py
@@ -27,16 +27,16 @@ class TestRootMeanSquare:
         actual = rms(orig, dim='x').values
         npt.assert_equal(actual, expected)
 
-    def test_reduce_2d(self):
+    @pytest.mark.parametrize('dim, axis', [('t', 1), ('x', 0)])
+    def test_reduce_2d(self, dim, axis):
         dat = np.array([[5, 7, 3.2, -1, -4.4], [-1, -2.5, 0, 8, 3.0]])
         orig = DataArray(dat, dims=['x', 't'])
-        axis = 1
         sum_squares = np.sum(dat**2, axis=axis)
         mean_squares = sum_squares / dat.shape[axis]
         rootmeansquare = np.sqrt(mean_squares)
 
         expected = rootmeansquare
-        actual = rms(orig, dim='t').values
+        actual = rms(orig, dim=dim).values
         npt.assert_equal(actual, expected)
 
     def test_reduce_2d_dask(self):
