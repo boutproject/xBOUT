d564a99201be679406db1651b582743aa5a55eab
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2019-09-12 20:00:33 +0100

    Test file for animate1D and 2D

---
+++ xbout/tests/test_animate.py
@@ -0,0 +1,34 @@
+import pytest
+from os import remove
+
+from xbout import open_boutdataset
+from xbout.boutdataarray import BoutDataArrayAccessor
+
+DATA_PATH = './data/dump_files/along_x/BOUT.dmp.*.nc'
+
+class TestAnimate:
+    """
+    Set of tests to check whether animate1D() and animate2D() are running properly
+    and PillowWriter is saving each animation correctly
+    """
+    def test_animate2D(self):
+
+        bd = open_boutdataset(DATA_PATH).squeeze(drop=True)
+
+        anim_creator = str(bd['T'].bout.animate2D(y='z'))
+        checker = '<animatplot.blocks.image_like.Imshow' in anim_creator    
+        
+        remove('./T_over_t.gif')
+        assert checker == True
+
+    def test_animate1D(self):
+
+        bd = open_boutdataset(DATA_PATH).squeeze(drop=True)
+
+        anim_creator = str(bd['T'][:,:,0].bout.animate1D())
+        print(anim_creator)
+        checker = '<animatplot.blocks.lineplots.Line' in anim_creator    
+        
+        remove('./T_over_t.gif')
+        assert checker == True 
+
