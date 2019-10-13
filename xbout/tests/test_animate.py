09d526e7cc9889f45e9ca8e532fa290a85c5948b
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2019-10-13 11:50:55 +0100

    Improved use of python functions for pytest and added use of pytest fixture to create temporary directory to deal with the output of subsequent animate function

--- xbout/tests/test_animate.py
+++ xbout/tests/test_animate.py
@@ -1,33 +1,36 @@
 import pytest
-from os import remove
 
 from xbout import open_boutdataset
 from xbout.boutdataarray import BoutDataArrayAccessor
 
-DATA_PATH = './xbout/tests/data/dump_files/along_x/BOUT.dmp.*.nc'
+from animatplot.blocks import Imshow, Line
 
+DATA_PATH = './xbout/tests/data/dump_files/along_x/BOUT.dmp.*.nc' # Path to test dmp files
+
+@pytest.fixture
+def create_test_file(tmpdir_factory):
+
+    save_dir = tmpdir_factory.mktemp("test_data") # Create temp dir for output of animate1D/2D
+    ds = open_boutdataset(DATA_PATH).squeeze(drop=True) # Open test data
+
+    return save_dir, ds
 
 class TestAnimate:
     """
     Set of tests to check whether animate1D() and animate2D() are running properly
     and PillowWriter is saving each animation correctly
     """
-    def test_animate2D(self):
+    def test_animate2D(self, create_test_file):
 
-        bd = open_boutdataset(DATA_PATH).squeeze(drop=True)
+        save_dir, ds = create_test_file
+        animation = ds['T'].bout.animate2D(y='z', save_as="%s/test.gif" % save_dir)
 
-        anim_creator = str(bd['T'].bout.animate2D(y='z'))
-        checker = '<animatplot.blocks.image_like.Imshow' in anim_creator
+        assert isinstance(animation, Imshow)
 
-        remove('./T_over_t.gif')
-        assert checker
+    def test_animate1D(self, create_test_file):
 
-    def test_animate1D(self):
+        save_dir, ds = create_test_file
+        animation = ds['T'][:, :, 0].bout.animate1D(save_as="%s/test.gif" % save_dir)
 
-        bd = open_boutdataset(DATA_PATH).squeeze(drop=True)
+        assert isinstance(animation, Line)
 
-        anim_creator = str(bd['T'][:, :, 0].bout.animate1D())
-        checker = '<animatplot.blocks.lineplots.Line' in anim_creator
-
-        remove('./T_over_t.gif')
-        assert checker
