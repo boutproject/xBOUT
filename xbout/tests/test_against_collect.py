6d39a4799d1d9291dd6157dbcc3db6ca9d9e9ff0
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2019-10-29 15:16:57 +0000

    Holding commit many more edits to be made - z-dim indexing option updated and tests added (tests will fail if xguards and yguards parameters change, issue being looked at in #61)

--- xbout/tests/test_against_collect.py
+++ xbout/tests/test_against_collect.py
@@ -2,41 +2,60 @@ import pytest
 
 import numpy.testing as npt
 
-from xbout.load import _auto_open_mfboutdataset
+from xbout import open_boutdataset
+from .test_load import create_bout_ds_list
+
+from xbout.load import collect as new_collect
+from boutdata import collect as old_collect
+
+
+@pytest.fixture
+def create_test_file(tmpdir_factory):
+    def _foo(nxpe, nype):
+        # Create temp dir for test files
+        save_dir = tmpdir_factory.mktemp("test_data")
+
+        # Generate test data
+        ds_list, file_list = create_bout_ds_list("data", nxpe=nxpe, nype=nype,
+                                             syn_data_type="linear")
+
+        for ds, file_name in zip(ds_list, file_list):
+            ds.to_netcdf(str(save_dir.join(str(file_name))))
+
+        return save_dir
+    return _foo
 
 
 class TestAccuracyAgainstOldCollect:
-    @pytest.mark.skip
-    def test_single_file(self):
-        from boutdata import collect
+    # @pytest.mark.skip
+    def test_single_file(self, create_test_file):
+
+        save_dir = create_test_file(nxpe=1,nype=1)
+
         var = 'n'
-        expected = collect(var, path='./tests/data/dump_files/single',
-                           prefix='equilibrium', xguards=False)
+        expected = old_collect(var, path=save_dir, prefix='data', xguards=False)
 
-        ds, metadata = _auto_open_mfboutdataset('./tests/data/dump_files/single/equilibrium.nc')
-        print(ds)
-        actual = ds[var].values
-
-        assert expected.shape == actual.shape
-        npt.assert_equal(actual, expected)
-
-    @pytest.mark.skip
-    def test_multiple_files_along_x(self):
-        from boutdata import collect
-        var = 'n'
-        expected = collect(var, path='./tests/data/dump_files/',
-                           prefix='BOUT.dmp', xguards=False)
-
-        ds, metadata = _auto_open_mfboutdataset('./tests/data/dump_files/BOUT.dmp.*.nc')
-        actual = ds[var].values
+        actual = new_collect(var, path=save_dir, prefix='data', xguards=False)
 
         assert expected.shape == actual.shape
         npt.assert_equal(actual, expected)
 
 
-    @pytest.mark.skip
-    def test_multiple_files_along_x(self):
-        ...
+   # @pytest.mark.skip
+    def test_multiple_files_along_x(self, create_test_file):
+
+        save_dir = create_test_file(nxpe=3, nype=3)
+
+        var = 'n'
+        expected = old_collect(var, path=save_dir, prefix='data', xguards=False)
+
+        actual = new_collect(var, path=save_dir, prefix='data', xguards=False)
+
+        print(expected.shape, actual.shape)
+
+        assert expected.shape == actual.shape
+        npt.assert_equal(actual, expected)
+
 
     @pytest.mark.skip
     def test_metadata(self):
