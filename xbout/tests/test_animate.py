7003f704c71edfced74a49c4f6eded5471706335
John Omotani <john.omotani@ukaea.uk>
2019-12-04 13:36:34 +0000

    Extend test_animate2D to check default dimensions work correctly
    animate2D should work for any (2 space)*(1 time) dimensional array,
    without 'x' and 'y' arguments being set (they can still be used for
    example to change transpose the plot if desired). This commit updates
    the test_animate2D() to verify that this works for arrays with y-z, x-z
    and x-y dimensions.

--- xbout/tests/test_animate.py
+++ xbout/tests/test_animate.py
@@ -32,7 +32,9 @@ class TestAnimate:
     def test_animate2D(self, create_test_file):
 
         save_dir, ds = create_test_file
-        animation = ds['n'].isel(y=1).bout.animate2D(y='z', save_as="%s/test" % save_dir)
+        animation = ds['n'].isel(x=1).bout.animate2D(save_as="%s/testyz" % save_dir)
+        animation = ds['n'].isel(y=2).bout.animate2D(save_as="%s/testxz" % save_dir)
+        animation = ds['n'].isel(z=3).bout.animate2D(save_as="%s/testxy" % save_dir)
 
         assert isinstance(animation, Imshow)
 
