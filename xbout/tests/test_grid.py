9fdec4139bcc55f49e04b85d034d9efff86dc3bf
John Omotani <john.omotani@ukaea.uk>
2020-03-20 21:58:46 +0000

    Include 'dy' in all inputs for tests
    'dy' now required for open_boutdataset

--- xbout/tests/test_grid.py
+++ xbout/tests/test_grid.py
@@ -20,7 +20,8 @@ def create_example_grid_file(tmpdir_factory):
 
     # Create grid dataset
     arr = np.arange(6).reshape(2, 3)
-    grid = DataArray(data=arr, dims=['x', 'y'])
+    grid = DataArray(data=arr, name='arr', dims=['x', 'y']).to_dataset()
+    grid['dy'] = DataArray(np.ones((2, 3)), dims=['x', 'y'])
 
     # Create temporary directory
     save_dir = tmpdir_factory.mktemp("griddata")
