d06e38fd2b317334f1ab58d06f0dd80c2ee67d2f
John Omotani <john.omotani@ukaea.uk>
2020-03-24 13:15:36 +0000

    Extra test for interpolate_parallel

--- xbout/tests/test_boutdataarray.py
+++ xbout/tests/test_boutdataarray.py
@@ -391,6 +391,46 @@ class TestBoutDataArrayMethods:
         npt.assert_allclose(n_highres.values, expected.values,
                             rtol=0., atol=1.1e-2)
 
+    def test_interpolate_parallel_sol(self, tmpdir_factory, bout_xyt_example_files):
+        path = bout_xyt_example_files(tmpdir_factory, lengths=(2, 3, 16, 3), nxpe=1,
+                                      nype=1, nt=1, grid='grid', guards={'y': 2},
+                                      topology='sol')
+
+        ds = open_boutdataset(datapath=path,
+                              gridfilepath=Path(path).parent.joinpath('grid.nc'),
+                              geometry='toroidal', keep_yboundaries=True)
+
+        n = ds['n']
+
+        thetalength = 2.*np.pi
+
+        dtheta = thetalength/16.
+        theta = xr.DataArray(np.linspace(0. - 1.5*dtheta, thetalength + 1.5*dtheta, 20),
+                             dims='theta')
+
+        dtheta_fine = thetalength/128.
+        theta_fine = xr.DataArray(
+                np.linspace(0. + 0.5*dtheta_fine, thetalength - 0.5*dtheta_fine, 128),
+                dims='theta')
+        x = xr.DataArray(np.arange(3), dims='x')
+
+        def f_y(t):
+            t = np.sin(t)
+            return (t**3 - t**2 + t - 1.)
+
+        f = f_y(theta) * (x + 1.)
+
+        n.data = f.broadcast_like(n)
+
+        f_fine = f_y(theta_fine)*(x + 1.)
+
+        n_highres = n.bout.interpolate_parallel().isel(theta=slice(2, -2))
+
+        expected = f_fine.broadcast_like(n_highres)
+
+        npt.assert_allclose(n_highres.values, expected.values,
+                            rtol=0., atol=1.1e-2)
+
     def test_interpolate_parallel_toroidal_points(self, tmpdir_factory,
                                                   bout_xyt_example_files):
         path = bout_xyt_example_files(tmpdir_factory, lengths=(2, 3, 16, 3), nxpe=1,
