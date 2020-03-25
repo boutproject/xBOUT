c4004e9b757a907e802c895751232e40d40aa119
John Omotani <john.omotani@ukaea.uk>
2020-03-25 10:08:47 +0000

    Update Region.connection_* variables in plotting/utils.py

--- xbout/plotting/utils.py
+++ xbout/plotting/utils.py
@@ -87,7 +87,7 @@ def plot_separatrices(da, ax):
     ycoord = da0.metadata['bout_ydim']
 
     for da_region in da_regions.values():
-        inner = da_region.region.connection_inner
+        inner = da_region.region.connection_inner_x
         if inner is not None:
             da_inner = da_regions[inner]
             R = 0.5*(da_inner['R'].isel(**{xcoord: -1})
@@ -116,14 +116,14 @@ def plot_targets(da, ax, hatching=True):
         y_boundary_guards = 0
 
     for da_region in da_regions.values():
-        if da_region.region.connection_lower is None:
+        if da_region.region.connection_lower_y is None:
             # lower target exists
             R = da_region.coords['R'].isel(**{ycoord: y_boundary_guards})
             Z = da_region.coords['Z'].isel(**{ycoord: y_boundary_guards})
             [line] = ax.plot(R, Z, 'k-', linewidth=2)
             if hatching:
                 _add_hatching(line, ax)
-        if da_region.region.connection_upper is None:
+        if da_region.region.connection_upper_y is None:
             # upper target exists
             R = da_region.coords['R'].isel(**{ycoord: -y_boundary_guards - 1})
             Z = da_region.coords['Z'].isel(**{ycoord: -y_boundary_guards - 1})
