dfab775981c696f100c15ffcdad878aae4939057
John Omotani <john.omotani@ukaea.uk>
2020-07-30 14:52:46 +0100

    Update minimum dask to minimum version supported by xarray-0.16.0
    xarray requires less-than-6-months old dask, so 0.16.0 requires
    dask-2.10.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         'xarray>=0.16.0',
-        'dask[array]>=1.0.0',
+        'dask[array]>=2.10.0',
         'natsort>=5.5.0',
         'matplotlib>=3.1.1',
         'animatplot>=0.4.1',
