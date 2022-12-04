90e532d53b4962a0bd9756cc07854211868adad5
John Omotani <john.omotani@ukaea.uk>
2022-12-04 16:51:55 +0000

    Only check BOUT_VERSION for dump or restart files
    BOUT_VERSION does not have to be defined for grid files (as these are
    produced by hypnotoad, not BOUT++).

--- xbout/load.py
+++ xbout/load.py
@@ -380,7 +380,9 @@ but we did load {grididfile}."""
     # BOUT++
     ds.bout.fine_interpolation_factor = 8
 
-    if ds.metadata["BOUT_VERSION"] < 4.0:
+    if ("dump" in input_type or "restart" in input_type) and ds.metadata[
+        "BOUT_VERSION"
+    ] < 4.0:
         # Add workarounds for missing information or different conventions in data saved
         # by BOUT++ v3.x.
         for v in ds:
