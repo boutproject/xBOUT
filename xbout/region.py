420415cb322956fa6b5f0c3dfc9abc2db242054f
Mike Kryjak <mikekryjak@gmail.com>
2023-03-16 14:19:14 +0000

    Fix typo on topology metadata

--- xbout/region.py
+++ xbout/region.py
@@ -1220,7 +1220,7 @@ def _create_regions_toroidal(ds):
     _check_connections(regions)
 
     ds = _set_attrs_on_all_vars(ds, "regions", regions)
-    ds["topology"] = topology
+    ds.metadata["topology"] = topology
     
     return ds
 
