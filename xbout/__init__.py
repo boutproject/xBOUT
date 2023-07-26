9ff009b1a1f708bf02f9b69557900b15bc3ba1ee
Peter Hill <peter.hill@york.ac.uk>
2023-07-26 15:14:55 +0100

    Add some imports to `__all__`

--- xbout/__init__.py
+++ xbout/__init__.py
@@ -19,3 +19,17 @@ except PackageNotFoundError:
     from setuptools_scm import get_version
 
     __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = [
+    "open_boutdataset",
+    "collect",
+    "geometries",
+    "register_geometry",
+    "REGISTERED_GEOMETRIES",
+    "BoutDataArrayAccessor",
+    "BoutDatasetAccessor",
+    "animate_pcolormesh",
+    "animate_poloidal",
+    "plot_separatrix",
+    "open_fastoutput",
+]
