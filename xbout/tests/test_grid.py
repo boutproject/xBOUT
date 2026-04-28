cb3801f0133042c7f133da04887c361a53aacb57
David Bold <dave@ipp.mpg.de>
2026-04-28 09:59:55 +0200

    Suppress all warnings

--- xbout/tests/test_grid.py
+++ xbout/tests/test_grid.py
@@ -55,10 +55,11 @@ class TestOpenGrid:
         merge([example_grid, new_var]).to_netcdf(dodgy_grid_path, engine="h5netcdf")
 
         with pytest.warns(
-            UserWarning, match="drop all variables containing " "the dimensions 'w'"
+            UserWarning, match="drop all variables containing the dimensions 'w'"
         ):
-            with open_boutdataset(datapath=dodgy_grid_path) as result:
-                result = result.drop_vars(["x", "y"])
+            with pytest.warns(UserWarning, match="o geometry type fou"):
+                with open_boutdataset(datapath=dodgy_grid_path) as result:
+                    result = result.drop_vars(["x", "y"])
         assert_equal(result, example_grid)
         example_grid.close()
         result.close()
