5ba64894dc0e035d3eeb9a7005cf7d50462b1014
mikekryjak <62797494+mikekryjak@users.noreply.github.com>
2026-05-19 16:36:44 +0100

    Removed orphaned comment
    Co-authored-by: David Bold <dave@ipp.mpg.de>

--- xbout/load.py
+++ xbout/load.py
@@ -1153,8 +1153,6 @@ def _open_grid(datapath, chunks, keep_xboundaries, keep_yboundaries, mxg=2, **kw
 
     unrecognised_dims = list(set(grid.dims) - set(acceptable_dims))
     if len(unrecognised_dims) > 0:
-        # Weird string formatting is a workaround to deal with possible bug in
-        # pytest warnings capture - doesn't match strings containing brackets
         grid = grid.drop_dims(unrecognised_dims)
 
     if keep_xboundaries:
